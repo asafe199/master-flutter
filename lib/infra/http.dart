9bd5ace2d02e6c19ac46525b3a779443c37b6e76
Asafe <asafe.anjos@capgemini.com>
2021-08-30 17:57:20 -0300

    [implementation / services / screens]

--- lib/infra/http.dart
+++ lib/infra/http.dart
@@ -10,7 +10,7 @@ class Connection {
   Map<dynamic, dynamic> data;
   String _url;
   int timeout;
-  Map<String, dynamic> headers;
+  Map<String, String> headers;
 
   Connection(this._http, this._url, {this.data, this.timeout, this.headers});
 
@@ -35,27 +35,27 @@ class Connection {
 
   Future<ResponseService> _get() async {
     IOClient http = _connectHttp();
-    var response = await http.get(Uri.parse(this._url), headers: this.headers);
+    var response = await http.get(Uri.parse(this._url), headers: headersDefault());
     return returnData(response);
   }
 
   Future<ResponseService> _delete() async {
     IOClient http = _connectHttp();
-    var response = await http.delete(Uri.parse(this._url), headers: this.headers);
+    var response = await http.delete(Uri.parse(this._url), headers: headersDefault());
     return returnData(response);
   }
 
   Future<ResponseService> _put() async {
     IOClient http = _connectHttp();
     var data = convert.json.encode(this.data);
-    var response = await http.put(Uri.parse(this._url), headers: headers, body: data, encoding: convert.Encoding.getByName("utf-8"));
+    var response = await http.put(Uri.parse(this._url), headers: headersDefault(), body: data, encoding: convert.Encoding.getByName("utf-8"));
     return returnData(response);
   }
 
   Future<ResponseService> _post() async {
     IOClient http = _connectHttp();
     var data = convert.json.encode(this.data);
-    var response = await http.post(Uri.parse(this._url), headers: this.headers, body: data, encoding: convert.Encoding.getByName("utf-8"));
+    var response = await http.post(Uri.parse(this._url), headers: headersDefault(), body: data, encoding: convert.Encoding.getByName("utf-8"));
     return returnData(response);
   }
 
@@ -66,7 +66,15 @@ class Connection {
 
   IOClient _connectHttp(){
     final client = HttpClient();
-    client.connectionTimeout = Duration(seconds: this.timeout != null ? 30 : this.timeout);
+    client.connectionTimeout = Duration(seconds: this.timeout == null ? 30 : this.timeout);
     return IOClient(client);
+  }
+
+  Map<String, dynamic> headersDefault(){
+    if(this.headers == null){
+      this.headers = Map();
+      this.headers['Content-type'] = "application/json; charset=utf-8";
+    }
+    return this.headers;
   }
 }
